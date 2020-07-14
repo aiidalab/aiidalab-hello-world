c8bb82373ff2ae34e6631a5a1708abcd0e1f47dd
Simon Adorf <simon.adorf@epfl.ch>
2020-07-14 14:00:05 +0200

    Remove expected fail tests and notebook.

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,11 +10,3 @@ def test_example(selenium, url):
     selenium.find_element(By.ID, 'notebook-container')
     selenium.find_element(By.CLASS_NAME, 'jupyter-widgets-view')
     selenium.get_screenshot_as_file('screenshots/example.png')
-
-
-@pytest.mark.xfail
-def test_example_import_error(selenium, url):
-    selenium.get(url('apps/apps/app/example-import-error.ipynb'))
-    selenium.find_element(By.ID, 'ipython-main-app')
-    selenium.find_element(By.ID, 'notebook-container')
-    selenium.find_element(By.CLASS_NAME, 'jupyter-widgets-view')
